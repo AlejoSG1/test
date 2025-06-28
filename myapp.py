1e24c618511d2a014081f120067a2422ed7a43be
Alejandro Salgado <169505010+alejosg12@users.noreply.github.com>
2025-06-28 17:46:52 -0500

    hola2

--- myapp.py
+++ myapp.py
@@ -91,4 +91,4 @@ def delete_contact(id):
     return jsonify({"message": "Contact deleted"})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001)
+    app.run(host="127.0.0.1", port=5000)
